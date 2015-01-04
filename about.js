ccd96bc2ec22ad1f33e02b339a1ec4b4e8d00a5d
SebRut <sebastian.rutofski@gmail.com>
2015-01-04 10:57:50 +0100

    first try

---
+++ about.js
@@ -0,0 +1,29 @@
+var friends = require("../../core/friends");
+//var logger = require('../../core/logger.js');
+var fs = require("fs");
+var readline = require("readline");
+
+exports.handle = function(input, source) {
+    var input = input.split(" ");
+
+    if(input[0] == "about") {
+        var about = "";
+        var package = JSON.parse(fs.readFileAsync("../../package.json"));
+
+        about += package["name"] + " v" + package["version"] + "\n";
+
+        friends.messageuser(source, about);
+
+        return true;
+    }
+
+    return false;
+}
+
+//exports.onExit() {
+//
+//}
+
+exports.getHelp = function() {
+  return "ABOUT\nabout - shows informations about this bot\n";
+}
